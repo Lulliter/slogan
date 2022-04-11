fbaeb9ea72f1056234b89e525857b8b16c241542
Lulliter <lmm76@georgetown.edu>
2022-04-11 22:32:16 +0200

    commit_as_of_2022-04-11

--- _build_deploy.sh
+++ _build_deploy.sh
@@ -7,14 +7,16 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # DEPLOY
 # When you build a static site, generated files are in _site directory (REPLACED WITH "docs")
 # [in R] clean local website
-Rscript -e "rmarkdown::clean_site(preview = FALSE)"
+#Rscript -e "rmarkdown::clean_site(preview = FALSE)"
+rm -rf ./docs/*
+
 # [in R] (re) build local website
 Rscript -e "rmarkdown::render_site(encoding = 'UTF-8')"
 
 # check
 git status
 # Stage
-git add -u # git add --all  # git add _build_deploy.sh
+git add -u # git add --all  # git add README.html
 # Commit (way 1)
 #git commit -m "Added Lexicon tab"
 # Commit (way 2)
