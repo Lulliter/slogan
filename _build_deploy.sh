0505bc4754273640664664b6161acd5ffa66f03d
Lulliter <lmm76@georgetown.edu>
2022-05-03 15:39:39 +0200

    rebuilding site Tue May  3 15:39:39 CEST 2022

--- _build_deploy.sh
+++ _build_deploy.sh
@@ -8,7 +8,11 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # When you build a static site, generated files are in _site directory (REPLACED WITH "docs")
 # [in R] clean local website
 #Rscript -e "rmarkdown::clean_site(preview = FALSE)"
-rm -rf ./docs/*
+
+#=====  (Clean Everiting *INSIDE* ./docs/*)
+setopt rmstarsilent # prevent from asking me permission
+rm -rf  docs/*
+unsetopt rmstarsilent
 
 # [in R] (re) build local website
 Rscript -e "rmarkdown::render_site(encoding = 'UTF-8')"
@@ -18,7 +22,7 @@ git status
 # Stage
 git add -u # git add --all  # git add README.html
 # Commit (way 1)
-#git commit -m "Added Lexicon tab"
+# git commit -m "worked on WRD from API" -m "Put work in _my_stuff/_01_data-overview.Rmd bc it exe But not knit  "
 # Commit (way 2)
           msg="rebuilding site `date`"
           if [ $# -eq 1 ]
@@ -27,3 +31,12 @@ git add -u # git add --all  # git add README.html
           git commit -m "$msg"
 # Push source and build repos.
 git push origin master
+
+
+
+
+
+
+#=========================================== (accidentally committed files to the repository) ================================================#
+git rm -r --cached _my_stuff/*  # followed by a commit and adding the file to .gitignore
+
