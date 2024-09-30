796d73b3748f66813c5bae16876dd0fb02ed7125
Lulliter <lmm76@georgetown.edu>
2024-09-30 19:47:08 +0200

    chgd analysis/01b_WB_project_pdo_anal.qmd & index.qmd 🔴🟠

--- ___render_deploy.sh
+++ ___render_deploy.sh
@@ -36,7 +36,7 @@ git status
 # Add changes to git Index.
 git add -A # ALL
 git add -u # tracked
-git add cv/*
+git add R/Colori.R
 git add images/*
 git add docs/*
 git add analysis/*
@@ -53,7 +53,7 @@ fi
 # ... Commit Those changes.
 git commit -m "$msg"
 =======
-git commit -m "chgd analysis/01b_WB_project_pdo_anal.qmd & _NO Q RENDER!_ analysis/_01a_WB_project_pdo_prep.qmd"
+git commit -m "chgd analysis/01b_WB_project_pdo_anal.qmd & hypotheses.qmd 🥸"
 git commit -m "major reorg structure" -m "see especially analysis/*"
 git commit -m "added analysis/* but mostly hidden "
 		# git commit -m "revision INSTALL + cleanup slides 2"  -m "01_... + 00_carico_tab-contesto.qmd "
