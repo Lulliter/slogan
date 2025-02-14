20afc03f7644155eeb422b4cc9b46ee587f7c787
Lulliter <lmm76@georgetown.edu>
2025-02-14 11:33:15 +0100

    small upd Home 👇🏻👇🏻

--- ___render_deploy.sh
+++ ___render_deploy.sh
@@ -63,7 +63,7 @@ git commit -m "$msg"
 =======
 git commit -m "fix analysis/01b_WB_project_pdo_EDA.qmd"
 git commit -m "major reorg structure" -m "see especially analysis/*"
-git commit -m "small upd Home"
+git commit -m "small upd Home 👇🏻"
 		# git commit -m "revision INSTALL + cleanup slides 2"  -m "01_... + 00_carico_tab-contesto.qmd "
 git commit -m "100% done blog  🍾🥂"
 
@@ -72,7 +72,7 @@ git commit -m "100% done blog  🍾🥂"
 git push origin master
 
 #=========================================== ALL IN ONE  ================================================#
-git add -u && git commit -a -m "small tweaks 2" && git push
+git add -u && git commit -a -m "small upd Home 👇🏻" && git push
 
 #=========================================== FIle pubblico  ================================================#
 # https://quarto.org/docs/publishing/quarto-pub.html
